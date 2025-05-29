f4bd210f9c85735badf8b83a89149ab44148688a
BViscay <brunoviscay@gmail.com>
2025-05-29 13:21:16 -0300

    app init

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,12 @@ import {createRoot} from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import ScrollToTop from "./Components/ScrollToTop.jsx";
 import {AuthProvider} from "./contexts/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
       <AuthProvider>
-        <ScrollToTop />
         <App />
       </AuthProvider>
     </StrictMode>
